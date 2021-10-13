ef3952e9e17a4a0f038eee66c6e80ab1ba7cf800
billy <braimbau@student.42.fr>
2021-10-13 12:06:59 +0200

    fix merge

--- include/User.hpp
+++ include/User.hpp
@@ -53,7 +53,7 @@ class User
 			return true;
 		}
 		void set_real_name(std::string realName) {
-			_real_name = realName;
+			_realName = realName;
 		}
 
 		std::string get_nick(void) const {
@@ -62,14 +62,7 @@ class User
 		std::string get_real_name(void) const {
 			return _realName;
 		}
-<<<<<<< HEAD
-=======
 
-		void set_real_name(std::string realName) {
-			_realName = realName;
-		}
-
->>>>>>> 481b58ddd507d4c5b94e506a80120cfbfa7fc5fd
 		int get_fd(void) const {
 			return _fd;
 		}
